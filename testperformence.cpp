2a036df98f2030da41d75df8034df76b27a0d223
sonne <sonne@localhost.localdomain>
2021-07-13 07:58:34 +0700

    first commit

---
+++ testperformence.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdio>
+#include <memory>
+#include <chrono>
+
+using namespace std;
+
+class Time
+{
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
+
+public:
+    Time()
+    {
+        m_StartTimepoint = std::chrono::high_resolution_clock::now();
+    }
+    ~Time()
+    {
+        Stop();
+    }
+    void Stop()
+    {
+        auto endTimepoint = std::chrono::high_resolution_clock::now();
+
+        auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+
+        auto duration = end - start;
+        double ms = duration * 0.001;
+        cout << "Duration: " << ms << " ms" << endl;
+    }
+};
